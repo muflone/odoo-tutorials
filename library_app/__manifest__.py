413fde551db6fcf86c481466b96743936e1475e9
Muflone <webreg@muflone.com>
2023-07-30 01:35:17 +0200

    Code cleanup

--- library_app/__manifest__.py
+++ library_app/__manifest__.py
@@ -1,4 +1,23 @@
-# -*- coding: utf-8 -*-
+##
+#     Project: Odoo Tutorials
+# Description: Odoo Tutorials
+#      Author: Fabio Castelli (Muflone) <muflone@muflone.com>
+#   Copyright: 2023 Fabio Castelli
+#     License: GPL-3+
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+##
+
 {
     'name': "Library Management",
     'description': """Manage library book catalogue and lending.""",
@@ -9,10 +28,6 @@
     'version': '0.1',
     'license': 'GPL-3',
     'website': 'https://github.com/muflone/odoo-tutorials',
-
-    # Categories can be used to filter modules in modules listing
-    # Check https://github.com/odoo/odoo/blob/12.0/odoo/addons/base/data/ir_module_category_data.xml
-    # for the full list
     'category': 'Uncategorized',
 
     # any module necessary for this one to work correctly
